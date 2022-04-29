8fe72e28f57b06596dc600198e31a9d9e535fc89
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-04-29 15:13:13 -0400

    First Commit of CatchPhrase Game

---
+++ catchPhrase.js
@@ -0,0 +1,18 @@
+/*  Create a while loop for the phrases so that it keeps showing new
+phrases after every answer until the Player 1 has reached 5 turns.*/
+
+/* create a prompt or form so the player can enter their answer after each phrase pops up*/
+
+
+/* Create a new while loop for Player 2 that runs 5 times */
+
+
+/* Create a function that compares the scores of Player 1 & 2 and gives highest score using Math.max
+Announces the winner with an alert */
+
+
+
+/*Play again button to restart the game*/
+
+
+
