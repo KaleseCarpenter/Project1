dd70da994cd59873a8eab9f59f2fe5795c7ba240
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-05-03 15:56:54 -0400

    trying to configure while loop for phrases

--- catchPhrase.js
+++ catchPhrase.js
@@ -4,28 +4,91 @@
 //     document.getElementById("letsPlay").innerHTML = "Hello World";
 //   }
 
+const catchPhrases = [
+    "When it rains____",
+    "It's raining ___ & ___", 
+    "Kill two birds ___",
+    "___ ___ the bush",
+    "___ ____ the doubt",
+    "It's ___ ____ science",
+    "__ ___, No gain",
+    "A ___ __ disguise", 
+    "Break __ ___",  
+    "A dime _ ___",
+];
+
+const playBtn = () => {
+    catchPhrases.forEach(element => {
+      document.getElementById('wordsScreen').innerHTML +=
+      `<div>${element}</div><br />`;
+      // here result is the id of the div present in the DOM
+   });
+};
+
+
 
 
 /*  Create a while loop for the phrases so that it keeps showing new
 phrases after every answer until the Player 1 has reached 5 turns.*/
 // random value generated with Math.random()
 
-​var catchPhrases = 'When it rains____',"It's raining ___ & ___", "Kill two birds ___", "___ ___ the bush",
+// const textScreen = document.querySelector(".textScreen").innerHTML = " ";
+// var catchPhrases = [
+//     "When it rains____",
+//     "It's raining ___ & ___", 
+//     "Kill two birds ___",
+//     "___ ___ the bush",
+//     "___ ____ the doubt",
+//     "It's ___ ____ science",
+//     "__ ___, No gain",
+//     "A ___ __ disguise", 
+//     "Break __ ___",  
+//     "A dime _ ___",
+    
+//     ]
+//     let phraseCaught = catchPhrases[Math.floor(Math.random() * catchPhrases.length)]
+//     console.log(phraseCaught)//This gives you any string from catchPhrases 
+
+
+
+
+
+/*​var catchPhrases = 'When it rains____',"It's raining ___ & ___", "Kill two birds ___", "___ ___ the bush",
      "___ ____ the doubt", "It's ___ ____ science", "__ ___, No gain",
      "A ___ __ disguise", "Break __ ___",  "A dime _ ___";
 var i=0;
 while (catchPhrases.length) {
   var phraseCaught = catchPhrases.pop();
   document.write("<p>" + phraseCaught + "</p>");
-}
-//The while loop loops through a block of code as long as a specified condition is true.
+}*/
 
-// ​let text = "";
-// let i = 0;
-// while (i < 10) {
-//   text += "phrases" + i;
-//   i++;
-// }
+
+
+
+
+
+/*The while loop loops through a block of code as long as a specified condition is true.*/
+
+
+​let text =  [
+    "When it rains____",
+    "It's raining ___ & ___", 
+    "Kill two birds ___",
+    "___ ___ the bush",
+    "___ ____ the doubt",
+    "It's ___ ____ science",
+    "__ ___, No gain",
+    "A ___ __ disguise", 
+    "Break __ ___",  
+    "A dime _ ___",
+    
+    ];
+let i = 0;
+while (i < 6) {
+    document.write(".text" + i);
+  text += "phrases" + i;
+  i++;
+}
 // document.getElementById("letsPlay").innerHTML = text;
 
 
