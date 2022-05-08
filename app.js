e833fc5eee1927b33d37b87a44a807f3206de6e4
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-05-08 19:10:56 -0400

    player one working

--- app.js
+++ app.js
@@ -2,135 +2,135 @@ let playerOne={
     score:0
 }
 // var randomPhrases =
-// let catchPhrases1 = {
-//         catchPhrases: "When it rains", 
-//         answer: "it pours" 
-// }    
+let catchPhrases1 = {
+        catchPhrases: "When it rains", 
+        answer: "it pours" 
+}    
 
-// let catchPhrases2 = {
-//         catchPhrases: "it's raining", 
-//         answer: "cats dogs"
-// }
+let catchPhrases2 = {
+        catchPhrases: "It's raining", 
+        answer: "cats and dogs"
+}
 
-// let catchPhrases3 = {
-//         catchPhrases: "kill two birds", 
-//         answer: "with one stone"
-// }
+let catchPhrases3 = {
+        catchPhrases: "Kill two birds", 
+        answer: "with one stone"
+}
 
-// let catchPhrases4 = {
-//         catchPhrases: "beating around", 
-//         answer: "the bush"
-// }
+let catchPhrases4 = {
+        catchPhrases: "Beating around", 
+        answer: "the bush"
+}
 
-// let catchPhrases5 = {
-//         catchPhrases: "benefit of", 
-//         answer:"the doubt"
-// }
+let catchPhrases5 = {
+        catchPhrases: "Benefit of", 
+        answer:"the doubt"
+}
 
-// let catchPhrases6 = {
-//         catchPhrases: "it's not _ science", 
-//         answer:"rocket"
-// }
+let catchPhrases6 = {
+        catchPhrases: "It's not _ science", 
+        answer:"rocket"
+}
 
-// let catchPhrases7 = {
-//         catchPhrases: "___ ___ no gain", 
-//         answer: "no pain"
-// }     
+let catchPhrases7 = {
+        catchPhrases: "___ ___ no gain", 
+        answer: "no pain"
+}     
 
-// let catchPhrases8 = {
-//         catchPhrases: "A blessing", 
-//         answer: "in disguise"
-// }
+let catchPhrases8 = {
+        catchPhrases: "A blessing", 
+        answer: "in disguise"
+}
 
-// let catchPhrases9 = {
-//         catchPhrases: "break a", 
-//         answer: "leg"
-// }
+let catchPhrases9 = {
+        catchPhrases: "break a", 
+        answer: "leg"
+}
 
-// let catchPhrases10 = {
-//         catchPhrases: "A dime", 
-//         answer: "a dozen"
-// }
+let catchPhrases10 = {
+        catchPhrases: "A dime", 
+        answer: "a dozen"
+}
 
-// let catchPhrases11 = {
-//         catchPhrases: "make a long", 
-//         answer: "story short"
-// }
+let catchPhrases11 = {
+        catchPhrases: "Make a long", 
+        answer: "story short"
+}
 
-// let catchPhrases12 = {
-//         catchPhrases: "you're pulling", 
-//         answer: "my leg"
-// }
+let catchPhrases12 = {
+        catchPhrases: "You're pulling", 
+        answer: "my leg"
+}
 
-// let catchPhrases13 = {
-//         catchPhrases: "feeling under", 
-//         answer: "the weather",
-// }
+let catchPhrases13 = {
+        catchPhrases: "Feeling under", 
+        answer: "the weather",
+}
 
-// let catchPhrases14 = {
-//         catchPhrases: "time flies when", 
-//         answer: "you're having fun"
-// }
+let catchPhrases14 = {
+        catchPhrases: "Time flies when", 
+        answer: "you're having fun"
+}
 
-// let catchPhrases15 = {
-//         catchPhrases: "that's __ ___ straw", 
-//         answer: "the last"
-// }
+let catchPhrases15 = {
+        catchPhrases: "That's __ ___ straw", 
+        answer: "the last"
+}
 
-// let catchPhrases16 = {
-//         catchPhrases: "off the", 
-//         answer: "hook"
-// }
+let catchPhrases16 = {
+        catchPhrases: "Off the", 
+        answer: "hook"
+}
 
-// let catchPhrases17 = {
-//         catchPhrases: "actions speak louder", 
-//         answer: "than words"
-// }
+let catchPhrases17 = {
+        catchPhrases: "Actions speak louder", 
+        answer: "than words"
+}
 
-// let catchPhrases18 = {
-//         catchPhrases: "bite off more than you", 
-//         answer: "can chew"
-// }
+let catchPhrases18 = {
+        catchPhrases: "Bite off more than you", 
+        answer: "can chew"
+}
 
-// let catchPhrases19 = {
-//         catchPhrases: "skating on", 
-//         answer: "thin ice"
-// }
+let catchPhrases19 = {
+        catchPhrases: "Skating on", 
+        answer: "thin ice"
+}
 
-// let catchPhrases20 = {
-//         catchPhrases: "on cloud", 
-//         answer: "nine"
-// }
+let catchPhrases20 = {
+        catchPhrases: "On cloud", 
+        answer: "nine"
+}
 
-// let catchPhrases21 = {
-//         catchPhrases: "through thick", 
-//         answer: "and thin"
-// }
+let catchPhrases21 = {
+        catchPhrases: "Through thick", 
+        answer: "and thin"
+}
 
-// let catchPhrases22 = {
-//         catchPhrases: "that ship", 
-//         answer: " has sailed"
-// }
+let catchPhrases22 = {
+        catchPhrases: "That ship", 
+        answer: " has sailed"
+}
 
-// let catchPhrases23 = {
-//         catchPhrases: "the pot calling", 
-//         answer: "the kettle black"
-// }
+let catchPhrases23 = {
+        catchPhrases: "The pot calling", 
+        answer: "the kettle black"
+}
 
-// let catchPhrases24 = {
-//         catchPhrases: "calm before", 
-//         answer: "the storm"
-// }
+let catchPhrases24 = {
+        catchPhrases: "Calm before", 
+        answer: "the storm"
+}
 
-// let catchPhrases25 = {
-//         catchPhrases: "curiosity killed the", 
-//         answer: "cat"
-// }
+let catchPhrases25 = {
+        catchPhrases: "Curiosity killed the", 
+        answer: "cat"
+}
 
-// let catchPhrases26 = {
-//         catchPhrases: "burning", 
-//         answer: "bridges"
-// }
+let catchPhrases26 = {
+        catchPhrases: "Burning", 
+        answer: "bridges"
+}
 
 let catchPhrases27 = {
         catchPhrases: "Head in the", 
@@ -183,5 +183,13 @@ let catchPhrases36 = {
 
 }
 
-let allCatchPhrases = [catchPhrases27, catchPhrases28, catchPhrases29,catchPhrases30, catchPhrases31, catchPhrases32, catchPhrases33, catchPhrases34, catchPhrases35, catchPhrases36]
+let allCatchPhrases = [catchPhrases1, catchPhrases2, catchPhrases3, catchPhrases4, 
+    catchPhrases5, catchPhrases6, catchPhrases7, catchPhrases8, catchPhrases9, catchPhrases10, 
+    catchPhrases11, catchPhrases12, catchPhrases13, catchPhrases14, catchPhrases15, 
+    catchPhrases16, catchPhrases17, catchPhrases18, catchPhrases19, catchPhrases20,
+    catchPhrases21, catchPhrases22, catchPhrases23, catchPhrases24, catchPhrases25, 
+    catchPhrases26, catchPhrases27, catchPhrases28, catchPhrases29,catchPhrases30, 
+    catchPhrases31, catchPhrases32, catchPhrases33, catchPhrases34, catchPhrases35,
+     catchPhrases36]
 
+     
--- catchPhrase.js
+++ catchPhrase.js
@@ -1,11 +1,7 @@
 // Create a function to start the game with the play button and display random phrase
 
-// const Player1 = document.getElementById("playerUno");
-//     Player1.addEventListener("click", playBtn);
-// const wordsScreen = document.getElementById("wordsScreen")
 
 // **********************Start Game****************************************
-// ******************START GAME*****************
 //  startGame() {
 // //   let text;
 // //    person = prompt("What is your name:", "Enter Your Name Here");
@@ -24,8 +20,8 @@
 
 
 
-
-const startGame = () =>{
+// ******************START GAME*****************
+const startGame = () => {
   //   let text;
   //  person = prompt("What is your name:", "Enter Your Name Here");
   //   if (person == null || person == "") {
@@ -38,69 +34,95 @@ const startGame = () =>{
   //     "<br>GOOD LUCK!";
   //   document.getElementById("para").innerHTML = text;
   // }
-  ques1 = prompt(catchPhrases27.catchPhrases);   //this grabs phrase 34's question
-  if (ques1 === catchPhrases27.answer){   // this returns the corresponding answer to question 34
+  ques1 = prompt(catchPhrases1.catchPhrases);   //this grabs phrase 34's question
+  if (ques1 === catchPhrases1.answer){   // this returns the corresponding answer to question 34
       alert("You got it!" + " +10 Points");
       addPoint() // this adds points to score after correct answer
       console.log(score) // to check to see if score is logging
   
   }else {
-      alert("Wrong Answer, Zero Points!" + "\nHead in the clouds");
+      alert("Wrong Answer, Zero Points!" + "\nWhen it rains it pours");
   }
   
-  ques2 = prompt(catchPhrases28.catchPhrases);
-  if (ques2 === catchPhrases28.answer){
+  ques2 = prompt(catchPhrases2.catchPhrases);
+  if (ques2 === catchPhrases2.answer){
       alert("You got it!" + " +10 Points");
       addPoint()
       console.log(score)
   }else {
-      alert("Wrong Answer, Zero Points!" + "\nNot playing with a full deck");
+      alert("Wrong Answer, Zero Points!" + "\nIt's raining cats and dogs");
   }
 
-  ques3 = prompt(catchPhrases29.catchPhrases);
-  if (ques3 === catchPhrases29.answer){
+  ques3 = prompt(catchPhrases3.catchPhrases);
+  if (ques3 === catchPhrases3.answer){
       alert("You got it!" + " +10 Points");
       addPoint()
       console.log(score)
   }else {
-      alert("Wrong Answer, Zero Points!" + "\nJump on the band wagon");
+      alert("Wrong Answer, Zero Points!" + "\nKill two birds with one stone");
   }
   
-  ques4 = prompt(catchPhrases30.catchPhrases);
-  if (ques4 === catchPhrases30.answer){
+  ques4 = prompt(catchPhrases4.catchPhrases);
+  if (ques4 === catchPhrases4.answer){
       alert("You got it!" + " +10 Points");
       addPoint()
       console.log(score)
   }else {
-      alert("Wrong Answer, Zero Points!" + "\nLike two peas in a pod");
+      alert("Wrong Answer, Zero Points!" + "\nBeating around the bush");
   }
 
-  ques5 = prompt(catchPhrases31.catchPhrases);
-  if (ques5 === catchPhrases31.answer){
+  ques5 = prompt(catchPhrases5.catchPhrases);
+  if (ques5 === catchPhrases5.answer){
       alert("You got it!" + " +10 Points");
       addPoint()
       console.log(score)
   }else {
-      alert("Wrong Answer, Zero Points!" + "\nThrough thick and thin");
+      alert("Wrong Answer, Zero Points!" + "\nBenefit of the doubt");
   }
       alert("G A M E  O V E R! \nIt's Player 2's turn");
-      // setTimeout(playerTwoTurn,1000)
+      // setTimeout(playerTwoTurn,3000)
+      alert("Player 2, Are you ready?");
 
+  //  ques6 = prompt(catchPhrases1.catchPhrases);
+  // if (ques6 === catchPhrases1.answer){
+  //     alert("You got it!" + " +10 Points");
+  //     addPoint()
+  //     console.log(score)
+  // }else {
+  //     alert("Wrong Answer, Zero Points!" + "\nWhen it rains it pours");
+  //     addPoint(twoScoreKeeper)
+  // }
+      
 }
 
+//******************Player 2 Questions*******************
 
 //***********************Score Keeper***************************
 
 let score = 0
-let scoreKeeper = document.querySelector('.p1Score')
+let oneScoreKeeper = document.querySelector('.p1Score')
+let twoScoreKeeper = document.querySelector(".pTwoScore");
 
 const addPoint = () => {
   
-  if (ques1 === catchPhrases27.answer) {
+  if (ques1 === catchPhrases1.answer) {
       // let relevantTarget = document.querySelector(".counter");
-  scoreKeeper.innerHTML = score += 10   
+  oneScoreKeeper.innerHTML = score += 10   
 }
+//   if (q1 === catchPhrases1.answer){
+//   twoScoreKeeper.innerHTML = += 10
+// }
 }
+
+
+// function playerTwoTurn() 
+
+
+
+
+
+
+
 
 
 
@@ -113,7 +135,7 @@ const addPoint = () => {
 //     false: 0
 // }
 
-// function resume() 
+// 
 
 
 
@@ -123,8 +145,6 @@ const addPoint = () => {
 // setTimeout(playerTwoTurn,1000)
 
 
-//******************Player 2 Questions*******************
-// function playerTwoTurn() 
 
 
 
@@ -163,118 +183,6 @@ const addPoint = () => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let catchPhrases = [
-//       { catchPhrases: "When it rains", answer: "it pours" },
-//       { catchPhrases: "it's raining", answer:  "cats dogs"},
-//       { catchPhrases: "kill two birds", answer: "with one stone"},
-//       { catchPhrases: "beating around", answer: "the bush"},
-//       { catchPhrases: "benefit of", answer:"the doubt"},
-//       { catchPhrases: "it's not _ science", answer:"rocket"},
-//       { catchPhrases: "___ ___ no gain", answer: "no pain"},
-//       { catchPhrases: "A blessing", answer: "in disguise"},
-//       { catchPhrases: "break a", answer: "leg"},
-//       { catchPhrases: "A dime", answer: "a dozen"},
-//       { catchPhrases: "make a long", answer: "story short"},
-//       { catchPhrases: "you're pulling", answer: "my leg"},
-//       { catchPhrases: "feeling under", answer: "the weather",
-//       { catchPhrases: "time flies when", answer: "you're having fun"},
-//       { catchPhrases: "that's __ ___ straw", answer: "the last"},
-//       { catchPhrases: "off the", answer: "hook"},
-//       { catchPhrases: "actions speak louder", answer: "than words"},
-//       { catchPhrases: "bite off more than you", answer: "can chew"},
-//       { catchPhrases: "skating on", answer: "thin ice"},
-//       { catchPhrases: "on cloud", answer: "nine"},
-//       { catchPhrases: "through thick", answer: "and thin"},
-//       { catchPhrases: "that ship", answer: " has sailed"},
-//       { catchPhrases: "the pot calling", answer: "the kettle black"},
-//       { catchPhrases: "calm before", answer: "the storm"},
-//       { catchPhrases: "curiosity killed the", answer: "cat"},
-//       { catchPhrases: "burning", answer: "bridges"},
-//       { catchPhrases: "head in the", answer: "clouds"},
-//       { catchPhrases: "not playing with a full", answer: "deck"},
-//       { catchPhrases: "jump on the band", answer: "wagon"},
-//       { catchPhrases: "like two peas", answer: "in a pod"},
-//       { catchPhrases: "through thick", answer: "and thin"},
-//       { catchPhrases: "it takes __ ___ tango", answer: "two to" },
-//       { catchPhrases: "straight from the horse's", answer: "mouth"},
-//       { catchPhrases: "every dog has", answer: "his day"},
-//       { catchPhrases: "keeping up with", answer: "the joneses"},
-//       { catchPhrases: "if it ain't broke", answer: "don't fix it"}
-
-//     ];
-
-// // const randomQuestions = () => {
-// //   const index = Math.floor(Math.random() * catchPhrases.length);
-// //   const question = catchPhrases.splice(index, 1);
-// //   return question[0];
-// //   question = prompt("Please enter your name:", "Harry Potter");
-
-// // }
-
-
-
-
-
-
 // while(catchPhrases.length) {
 //   const question = randomQuestions();
 //   const askedQuestion = prompt(question.question);
@@ -290,62 +198,10 @@ const addPoint = () => {
 
 
 
-// var gamesWon = 0;
-// var phraseGuesses =[]; // variable for current letters guessed
-// var gezWrd= []; // variable for current word I'm guessing
-// var usedPhrases = [];//variable to get NEW Phrase to guess
-// var phrasePredict; // stores the value for catchPhrases[Mathfloor & random]
-// var predictNum;// variable to store amount of guesses you get based on the length of the phrase
-
-// //Begin Playing
-// function playerStart() {
 
 
 
 
-// //Random word
-// /*  Create a loop for the phrases so that it keeps showing new
-// phrases after every answer until the Player 1 has reached 5 turns.*/
-// // random value generated with Math.random()
-// phrasePredict = catchPhrases[Math.floor(Math.random() * catchPhrases.length)].toUpperCase();
-
-// // num of guess (higher or lower) depending on phrase length
-// if (phrasePredict.length <= 5) {
-//   predictNum = 3
-// } else if (phrasePredict.length >5 && phrasePredict.length <= 7) {
-
-//   // multiply by .5
-//   predictNum = Math.floor(phrasePredict.length * .5)
-// } else if (phrasePredict.length >7 && phrasePredict.length <= 10) {
-//   predictNum = Math.floor(wordToMatch.length * .5)
-// } else if (phrasePredict.length >10 && phrasePredict.length <= 14) {
-//   predictNum = Math.floor(phrasePredict.length * .25)
-// } else if (phrasePredict.length >14) {
-//   predictNum = 8;
-// }
-
-/*var clicks = 0;
-    function playBtn() {
-      let phraseGuesses = Object.keys(catchPhrases)
-      // console.log it to see the answers
-      console.log(phraseGuesses)
-    let phraseCaught = phraseGuesses[Math.floor(Math.random() * phraseGuesses.length)]
-      wordsScreen.innerHTML= phraseCaught
-      //this is counting the amount of time the player clicks the button, it will disable the button after 5 turns
-      clicks += .5;
-      document.getElementById("pOneClicks").innerHTML = clicks;
-      //if amount of clicks is = to 5 then disable the button, it's player 2's turn.
-      if (clicks === 6) {
-      document.getElementById("playerUno").disabled = true;
-        let playerDosTurn = prompt(" Player 2's Turn. You Ready? ");
-
-        if (playerDosTurn === "yes,Yes") {
-      let nextPLayer = document.querySelector(".playerTwo");
-    }
-    // catchPhrases.forEach((element)=>{
-    //   wordsScreen.innerHTML += 
-    // })
-};*/
 
 // add counter to the Player1 button set = to 0 or 1, if counter = once they get to 5, next layer turn.
 // if counter === 5 diplayer 2 turn.
