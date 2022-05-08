78937d7ff69b773afd3551e300b14a4a0b46fc62
Kalese Carpenter <kalesecarpenter@gmail.com>
2022-05-08 09:55:57 -0400

    added new javascript page for updated object code

---
+++ app.js
@@ -0,0 +1,205 @@
+/*let pOneScore={
+    score:0
+}
+// var randomPhrases =
+// let catchPhrases1 = {
+//         catchPhrases: "When it rains", 
+//         answer: "it pours" 
+// }    
+
+// let catchPhrases2 = {
+//         catchPhrases: "it's raining", 
+//         answer: "cats dogs"
+// }
+
+// let catchPhrases3 = {
+//         catchPhrases: "kill two birds", 
+//         answer: "with one stone"
+// }
+
+// let catchPhrases4 = {
+//         catchPhrases: "beating around", 
+//         answer: "the bush"
+// }
+
+// let catchPhrases5 = {
+//         catchPhrases: "benefit of", 
+//         answer:"the doubt"
+// }
+
+// let catchPhrases6 = {
+//         catchPhrases: "it's not _ science", 
+//         answer:"rocket"
+// }
+
+// let catchPhrases7 = {
+//         catchPhrases: "___ ___ no gain", 
+//         answer: "no pain"
+// }     
+
+// let catchPhrases8 = {
+//         catchPhrases: "A blessing", 
+//         answer: "in disguise"
+// }
+
+// let catchPhrases9 = {
+//         catchPhrases: "break a", 
+//         answer: "leg"
+// }
+
+// let catchPhrases10 = {
+//         catchPhrases: "A dime", 
+//         answer: "a dozen"
+// }
+
+// let catchPhrases11 = {
+//         catchPhrases: "make a long", 
+//         answer: "story short"
+// }
+
+// let catchPhrases12 = {
+//         catchPhrases: "you're pulling", 
+//         answer: "my leg"
+// }
+
+// let catchPhrases13 = {
+//         catchPhrases: "feeling under", 
+//         answer: "the weather",
+// }
+
+// let catchPhrases14 = {
+//         catchPhrases: "time flies when", 
+//         answer: "you're having fun"
+// }
+
+// let catchPhrases15 = {
+//         catchPhrases: "that's __ ___ straw", 
+//         answer: "the last"
+// }
+
+// let catchPhrases16 = {
+//         catchPhrases: "off the", 
+//         answer: "hook"
+// }
+
+// let catchPhrases17 = {
+//         catchPhrases: "actions speak louder", 
+//         answer: "than words"
+// }
+
+// let catchPhrases18 = {
+//         catchPhrases: "bite off more than you", 
+//         answer: "can chew"
+// }
+
+// let catchPhrases19 = {
+//         catchPhrases: "skating on", 
+//         answer: "thin ice"
+// }
+
+// let catchPhrases20 = {
+//         catchPhrases: "on cloud", 
+//         answer: "nine"
+// }
+
+// let catchPhrases21 = {
+//         catchPhrases: "through thick", 
+//         answer: "and thin"
+// }
+
+// let catchPhrases22 = {
+//         catchPhrases: "that ship", 
+//         answer: " has sailed"
+// }
+
+// let catchPhrases23 = {
+//         catchPhrases: "the pot calling", 
+//         answer: "the kettle black"
+// }
+
+// let catchPhrases24 = {
+//         catchPhrases: "calm before", 
+//         answer: "the storm"
+// }
+
+// let catchPhrases25 = {
+//         catchPhrases: "curiosity killed the", 
+//         answer: "cat"
+// }
+
+// let catchPhrases26 = {
+//         catchPhrases: "burning", 
+//         answer: "bridges"
+// }
+
+// let catchPhrases27 = {
+//         catchPhrases: "head in the", 
+//         answer: "clouds"
+// }
+
+// let catchPhrases28 = {
+//         catchPhrases: "not playing with a full", 
+//         answer: "deck"
+// }
+
+// let catchPhrases29 = {
+//         catchPhrases: "jump on the band", 
+//         answer: "wagon"
+// }
+
+// let catchPhrases30 = {
+//         catchPhrases: "like two peas", 
+//         answer: "in a pod"
+// }
+
+// let catchPhrases31 = {
+//         catchPhrases: "through thick", 
+//         answer: "and thin"
+// }
+
+// let catchPhrases32 = {
+//         catchPhrases: "it takes __ ___ tango", 
+//         answer: "two to" 
+// }
+
+// let catchPhrases33 = {
+//         catchPhrases: "straight from the horse's", 
+//         answer: "mouth"
+// }
+
+let catchPhrases34 = {
+        catchPhrases: "every dog has", 
+        answer: "his day"
+}
+
+let catchPhrases35 = {
+        catchPhrases: "keeping up with", 
+        answer: "the joneses"
+}
+
+let catchPhrases36 = {
+        catchPhrases: "if it ain't broke", 
+        answer: "don't fix it"
+
+}
+
+let allCatchPhrases = [catchPhrases34,catchPhrases35,catchPhrases36]
+
+
+
+//get random word
+const randomPhrases = () =>{
+   allCatchPhrases = Math.floor(Math.random() * catchPhrases.length)
+   console.log(allCatchPhrases);
+} 
+
+const startGame = () =>{
+    ques1 = prompt(catchPhrases34.catchPhrases);
+    if (ques1 === catchPhrases34.answer){
+        alert("You got it!" + " +10 Points");
+    }else {
+        alert(
+        "Wrong Answer, Zero Points!" + "\nCorrect Answer: When it rains it pours");
+    }
+    pOneScore.score += 10
+}*/
